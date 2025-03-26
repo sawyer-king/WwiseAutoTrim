55dd8d2c16874e281d3ed7405b3773a0dd584494
Sawyer King <sking@vfs.com>
2025-03-26 16:32:23 -0700

    Added basic structure and comments for additions needed

--- delete-blankspace.py
+++ delete-blankspace.py
@@ -1,3 +1,28 @@
 # Copyright 2025 Sawyer King
 
-from pywwise import
+from pywwise import *
+
+
+def main():
+    """Check all .wav files in use in the Wwise project for blank space at the beginning or end of the file and
+    remove that blank space."""
+    ak = new_waapi_connection()
+
+    query = WaqlQuery()
+    query.from_project()  # This will  get every object in the Wwise project.
+
+    wwise_objects = ak.wwise.core.object.get(query)
+
+    ak.wwise.core.undo.begin_group()
+
+    for obj in wwise_objects:
+        if pqweokperoqwk in obj.name:  # If blank space in wav files NEED TO ADD/CHANGE.
+            new_file = obj.other  # replace OR EDIT with fixed file
+
+    ak.wwise.core.undo.end_group("Delete Blankspace")
+
+    ak.disconnect()
+
+
+if __name__ == "__main__":
+    main()
