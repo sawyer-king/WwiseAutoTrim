ebd237a46afc05dc87d2fc7af04bc819ac1a70b1
Sawyer King <sking@vfs.com>
2025-04-12 16:01:46 -0700

    Added documentation and rough framework for converting 16/24bit audio to 32bit audio

--- delete_blankspace_all.py
+++ delete_blankspace_all.py
@@ -17,8 +17,12 @@ def main():
     ak.wwise.core.undo.begin_group()
 
     for obj in wwise_objects:
-        if pqweokperoqwk in obj.name:  # If blank space in wav files NEED TO ADD/CHANGE.
-            new_file = obj.other  # replace OR EDIT with fixed file
+        if obj.type == Sound:
+            if oiawjd in obj.name:
+                new_frames = audioop.lin2lin(frames, old_width, 4) # If blank space in wav files NEED TO ADD/CHANGE. https://stackoverflow.com/questions/44812553/how-to-convert-a-24-bit-wav-file-to-16-or-32-bit-files-in-python3
+                new_frames = audioop.bias(new_frames, 4, 128)#https://docs.python.org/3.10/library/audioop.html
+
+                new_file = obj.other  # replace OR EDIT with fixed file
 
     ak.wwise.core.undo.end_group("Delete Blankspace In All") # figure out where this display name is for
 
