75e79016733f30565ccc644f2ad307c57049fc42
Sawyer King <sawyer.audio@gmail.com>
2025-04-20 21:02:53 -0700

    Syntax/Spacing fixes

--- delete_blankspace_all.py
+++ delete_blankspace_all.py
@@ -14,8 +14,7 @@ def main():
     query = WaqlQuery()
     query.from_project()  # This will  get every object in the Wwise project.
 
-    wwise_objects = ak.wwise.core.object.get(
-        query)  # maybe try to get only audio files and not objects (sound containers?)
+    wwise_objects = ak.wwise.core.object.get(query)  # maybe try to get only audio files and not objects (sound containers?)
 
     ak.wwise.core.undo.begin_group()
 
@@ -37,9 +36,8 @@ def main():
             AudioSource.trim_end = trim_end
 
             if trim_begin > 0 or trim_end < num_samples - 1:
-                temp_dict = {'FileName':obj.name, 'Path':obj.path}
-                modified_files_list.append = temp_dict #make a list of all modified files to be printed in a display to the user
-
+                temp_dict = {'FileName': obj.name, 'Path': obj.path}
+                modified_files_list.append = temp_dict  # make a list of all modified files to be printed in a display to the user
 
     if len(modified_files_list) > 0:
         print(modified_files_list)  # also think about adding file name + folder path + Wwise actor mixer structure path
