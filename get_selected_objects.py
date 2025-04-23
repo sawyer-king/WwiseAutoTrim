483bcc019cbd506723d165e26f6ad29227aa7443
Sawyer King <sawyer.audio@gmail.com>
2025-04-23 08:41:49 -0700

    Added 2 examples scripts for GUI and selected info get

---
+++ get_selected_objects.py
@@ -0,0 +1,24 @@
+from pywwise import *
+
+
+def print_info(info: tuple[WwiseObjectInfo, ...]):
+    """
+    Print the name, ID, type, and path of a Wwise Object.
+    :param info: The `WwiseObjectInfo` containing the information to display.
+    """
+    for obj in info:
+        print(f"Name: {obj.name}")
+        print(f"GUID: {obj.guid}")
+        print(f"Type: {obj.type}")
+        print(f"Path: {obj.path}")
+        print()  # Empty line, for formatting purposes.
+
+
+def main():
+    """Set up a callback for object selections in Wwise."""
+    ak = new_waapi_connection()
+    ak.wwise.ui.selection_changed += print_info
+
+
+if __name__ == "__main__":
+    main()
