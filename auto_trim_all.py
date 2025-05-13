6c7df1c5991b7164eb920d6babc29dd3a7922465
Sawyer King <sawyer.audio@gmail.com>
2025-05-12 23:02:43 -0700

    Updated Docustring

--- auto_trim_all.py
+++ auto_trim_all.py
@@ -6,8 +6,8 @@ from scipy.io import wavfile
 
 def main():
     """Check all .wav files in use in the Wwise project for empty space at the beginning or end of the file and
-    set trim points to remove that empty space. Also includes a GUI component that gives the option to add
-    Initial Delay to the sound to keep the original sync offset."""
+    set trim points to remove that empty space. Also includes the option to add Initial Delay to the sound to keep
+    the original sync offset."""
     ak = new_waapi_connection()
 
     modified_files_list = []
